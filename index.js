5e50c9116e0533a09be5996e3f38a71aecf0fe2f
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-06 14:42:42 +0700

    Latihan 7.3 - express start dan listerner server

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-var http = require ('http');
+var express = require ('express');
+var app = express()
 
-http.createServer( (req,res) =>{
-    if (req.url == '/home')
-        res.write('<p>Tampilkan Halaman Home</p>')
-    else if (req.url == '/product')
-    res.write('<p>Tampilkan Halaman Product</p>')
-    else if (req.url == '/order')
-    res.write('<p>Tampilkan Halaman Order</p>')
-    else if (req.url == '/')
-    res.write('<p>Pengembangan Web Node js latihan 7.2</p>')
-    else
-    res.write('<h1>404 not found</h1>')
-    res.end();
-}).listen(8000, () =>{
+app.get('/', (req,res) =>{
+    res.end('konten dari method GET ')
+
+})
+
+app.listen(8000, () =>{
     console.log('server berjalan di port 8000')
 });
