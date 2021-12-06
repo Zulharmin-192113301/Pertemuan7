25bc0897ef6e66e691418e5042b6dbee5ebe1f77
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-06 15:07:11 +0700

    Latihan 7.6 - express method get route path

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ var express = require ('express');
 var app = express()
 
 app.get('/', (req,res) =>{
-    res.end('konten dari method GET ')
+    res.end('<h1>Halaman  Default</h1> ')
 
 })
 
-app.post('/', (req,res) =>{
-    res.end('konten dari method Post ')
+app.get('/home', (req,res) =>{
+    res.end('<h1>Halaman Home</h1> ')
 
 })
+
+app.get('/order', (req,res) =>{
+    res.end('<h1>Halaman Order</h1> ')
+
+})
+
+
 
 http.createServer(app)
 app.listen(8000, () =>{
