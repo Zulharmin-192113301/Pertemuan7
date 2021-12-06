88be69a54f33bb6b93b360ba3302d478ce35b671
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-06 14:21:41 +0700

    Latihan 7.2 - atribute url pada request

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var http = require ('http');
 
 http.createServer( (req,res) =>{
-    res.write ('<p>Pengembangan App Web dengan NodeJS Latihan 7.1</p>');
+    if (req.url == '/home')
+        res.write('<p>Tampilkan Halaman Home</p>')
+    else if (req.url == '/product')
+    res.write('<p>Tampilkan Halaman Product</p>')
+    else if (req.url == '/order')
+    res.write('<p>Tampilkan Halaman Order</p>')
+    else if (req.url == '/')
+    res.write('<p>Pengembangan Web Node js latihan 7.2</p>')
+    else
+    res.write('<h1>404 not found</h1>')
     res.end();
 }).listen(8000, () =>{
     console.log('server berjalan di port 8000')
