094e55f0fbcc1738985f684012ecb20ea769756b
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-06 14:13:14 +0700

    Latihan 7.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require ('http');
 
 http.createServer( (req,res) =>{
-    res.write ('<p>Pengembangan App Web dengan NodeJS</p>');
+    res.write ('<p>Pengembangan App Web dengan NodeJS Latihan 7.1</p>');
     res.end();
 }).listen(8000, () =>{
     console.log('server berjalan di port 8000')
